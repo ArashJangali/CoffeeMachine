03bef6cfd479dc8cef2a12426519ea0633355064
Arash Jangali <arashjangali@Arashs-MacBook-Pro.local>
2023-06-15 00:49:57 +0200

    First commit

--- main.py
+++ main.py
@@ -28,32 +28,38 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0,
 }
 
-machine_on = True
 
 
 def check(drink, paid_amount, chosen_drink):
     cost = drink["cost"]
     water = drink["ingredients"]["water"]
-    milk = drink["ingredients"]["milk"]
     coffee = drink["ingredients"]["coffee"]
 
     resource_water = resources["water"]
-    resource_milk = resources["milk"]
     resource_coffee = resources["coffee"]
 
+    if chosen_drink == "espresso":
+        milk = 0
+        resource_milk = 0
+    else:
+        milk = drink["ingredients"]["milk"]
+        resource_milk = resources["milk"]
+
     if paid_amount >= cost and resource_water >= water and resource_milk >= milk and resource_coffee >= coffee:
-        change = paid_amount - cost
+        change = round((paid_amount - cost), 2)
+        resources["water"] -= water
+        resources["coffee"] -= coffee
+        if chosen_drink != "espresso":
+            resources["milk"] -= milk
         make_coffee(chosen_drink, change)
-        resource_water = resource_water - water
-        resource_milk = resource_milk - milk
-        resource_coffee = resource_coffee - coffee
     elif paid_amount < cost:
         print("Sorry that's not enough money. Money refunded.")
     elif resource_water < water:
         print("Not enough water")
-    elif resource_milk < milk:
+    elif resource_milk < milk and drink != "espresso":
         print("Not enough milk")
     elif resource_coffee < coffee:
         print("Not enough coffee")
@@ -63,16 +69,21 @@ def check(drink, paid_amount, chosen_drink):
 
 def make_coffee(chosen_drink, change):
 
-
     print(f"Here is ${change} in change.")
     print(f"Here is your {chosen_drink} ☕️. Enjoy")
+    machine()
+
 
 def machine():
 
     chosen_drink = input("What would you like? (espresso/latte/cappuccino): ")
 
-    if chosen_drink == "report":
+    if chosen_drink == "off":
+        print("Turning off the machine.")
+        return
+    elif chosen_drink == "report":
         print("Water: ", resources["water"], "Milk: ", resources["milk"], "Coffee: ", resources["coffee"])
+        machine()
     elif chosen_drink == "latte" or chosen_drink == "espresso" or chosen_drink == "cappuccino":
         drink = MENU[chosen_drink]
         print("Please insert coins.")
@@ -87,4 +98,3 @@ def machine():
 
 
 machine()
-
